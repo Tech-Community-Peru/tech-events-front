df53159a9bf1e1d1a978fadf65b9b67903b3e1cf
Diego Sanchez Chuquimango <165108758+dsanchezchu@users.noreply.github.com>
2024-11-15 03:13:28 -0500

    Revert "Feature/register module"

--- src/app/shared/models/register-request.model.ts
+++
@@ -1,7 +0,0 @@
-export interface RegisterRequest{
-  nombre: string,
-  apellido: string,
-  correoElectronico: string,
-  password: string,
-  paisOrigen: string
-}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  product: true,
-  baseURL: 'https://techcommunityperu-api-latest.onrender.com/api/v1'
-};
