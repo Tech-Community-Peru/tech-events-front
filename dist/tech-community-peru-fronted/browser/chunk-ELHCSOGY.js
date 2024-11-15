14d71ed01d7e97efd0e60f3fd21451d4e36e4b28
Diego Sanchez Chuquimango <165108758+dsanchezchu@users.noreply.github.com>
2024-11-15 07:26:57 -0500

    despliegue netlify

---
+++ dist/tech-community-peru-fronted/browser/chunk-ELHCSOGY.js
@@ -0,0 +1,1 @@
+import{Ab as l,cb as a,db as o,fa as n,wb as i}from"./chunk-YJIIRPSP.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-event-detail"]],standalone:!0,features:[l],decls:2,vars:0,template:function(e,m){e&1&&(a(0,"p"),i(1,"event-detail works!"),o())}})};export{p as EventDetailComponent};
