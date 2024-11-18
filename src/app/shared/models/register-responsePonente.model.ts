e7ef817485793b4653a1e49442353d2e02db8ecb
LeoSanchez22 <lsanchezr29@upao.edu.pe>
2024-11-18 17:42:39 -0500

    feat(frontend) Registrarme como ponente

---
+++ src/app/shared/models/register-responsePonente.model.ts
@@ -0,0 +1,12 @@
+export interface RegisterResponsePonente {
+    paisOrigen: any;
+    id: number;
+    correoElectronico: string;
+    idRole: number;
+    nombreRole: string;
+    rolRole: string;
+    nombre: string;
+    apellido: string;
+    cargo: string;
+    especialidad: string;
+  }
